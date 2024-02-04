f6ba9ea90fff0d01bfcc46ce0472599a87db8f4a
bradleypelton <bradleypelton@gmail.com>
2024-02-04 10:28:15 -0800

    fixing log4j2 logs, removing starter-web dependency

--- src/main/java/com/starrydecisis/stardec/StardecApplication.java
+++ src/main/java/com/starrydecisis/stardec/StardecApplication.java
@@ -2,13 +2,18 @@ package com.starrydecisis.stardec;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Arrays;
 
 
 @SpringBootApplication
@@ -20,5 +25,26 @@ public class StardecApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StardecApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			logger.debug("================================================");
+			logger.debug("================================================");
+			logger.debug("================================================");
+			logger.debug("CURRENT BEANS:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				logger.debug(beanName);
+			}
+			logger.debug("================================================");
+			logger.debug("================================================");
+			logger.debug("================================================");
+
+		};
 	}
 }
